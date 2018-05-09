4b8c1d076fbed57b3cbb6dd3b434d243924a4e61
Joshua Chin <joshuarchin@gmail.com>
2018-05-09 16:39:52 -0400

    added batch drawing

--- minecraft.py
+++ minecraft.py
@@ -6,6 +6,14 @@ class Model(object):
     def __init__(self):
         self.batch = pyglet.graphics.Batch()
 
+        x,y,z = 0,0,0
+        X,Y,Z = x+1,y+1,z+1
+
+        color = (1,1,1) * 4
+
+        self.batch.add(4, GL_QUADS, None,
+            ('v3f', [x,y,z, X,y,z, X,Y,z, x,Y,z]), color)
+
     def draw(self):
         self.batch.draw()
 
@@ -16,6 +24,9 @@ class Window(pyglet.window.Window):
         self.set_minimum_size(400, 300)
         glClearColor(.52, .80, .91, 1)
         self.model = Model()
+
+    def set3d(self):
+        pass
 
     def on_draw(self):
         self.clear()
