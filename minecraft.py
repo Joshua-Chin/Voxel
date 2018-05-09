8a9fb0d304f66302e65f7819b4651d5c60866d41
Joshua Chin <joshuarchin@gmail.com>
2018-05-09 16:45:54 -0400

    added set2d

--- minecraft.py
+++ minecraft.py
@@ -25,12 +25,23 @@ class Window(pyglet.window.Window):
         glClearColor(.52, .80, .91, 1)
         self.model = Model()
 
-    def set3d(self):
+    def projection(self):
         glMatrixMode(GL_PROJECTION)
-        glLoadIdentity()
-        gluPerspective(70, self.width/self.height, 0.5, 1000)
+        glLoadIdentity() 
+    
+    def modelview(self):
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
+
+    def set3d(self):
+        self.projection()
+        gluPerspective(70, self.width/self.height, 0.5, 1000)
+        self.modelview()
+
+    def set2d(self):
+        self.projection()
+        gluOrtho2D(0, self.width, 0, self.height)
+        self.modelview()
 
     def on_draw(self):
         self.clear()
